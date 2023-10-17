413acaa5ea587fcddf55619de53dee8973cfa26a
kobrius11 <kobrius11@gmail.com>
2023-10-17 23:52:49 +0300

    pagination works !

--- website/main/views.py
+++ website/main/views.py
@@ -15,42 +15,40 @@ def download(request):
             item.streams.get_audio_only().download()
         return HttpResponse('<script type="text/javascript">window.close()</script>')
 
+
 class ListView(generic.ListView):
-    search_query = forms.SearchQuary
     template_name = "index.html"
-    paginate_by = 50
+    paginate_by = 25
     queryset = None
     
     @property
     def get_search_query(self):
-        return self.request.GET.get("search")
+        return self.request.GET.get("query")
     
+    @property
     def get_page(self):
-        if self.request.GET.get('page'):
-            print(self.request.GET.get('page'))
-            return int(self.request.GET.get('page'))
-        print(self.request.GET.get('page'))
+        if self.request.GET.get("page"):
+            return int(self.request.GET.get("page"))
         return 1
-            
+    
+    @property
     def is_list(self):
         return 'list' in self.get_search_query # self.request.GET.get("search").__contains__("list")
     
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data()
-        return render(request, self.template_name, context)
-    
     def get_queryset(self):
-        if self.get_search_query():
-            if self.is_list():
+        if self.get_search_query:
+            if self.is_list:
                 self.queryset = Playlist(self.get_search_query)
             else:
                 self.queryset = self.get_search_query
         return self.queryset
     
     def get_context_data(self, **kwargs) -> dict[str, Any]:
-        context = super().get_context_data(object_list = self.get_queryset(), **kwargs)
-        context["object_list"] = context["paginator"].page(self.get_page()).object_list
-        context["url"] = self.request.get_full_path()
-        return context
+        if self.get_queryset():
+            context = super().get_context_data(**kwargs)
+            len(context["page_obj"].object_list) * self.get_page
+            context["object_list"] = self.get_queryset().videos[(len(context["page_obj"].object_list) * (self.get_page -1)): (len(context["page_obj"].object_list) * self.get_page)]
+            return context
 
-        
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        return super().get(request, *args, **kwargs)
